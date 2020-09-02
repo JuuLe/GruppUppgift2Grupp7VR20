3b2bdc0acc164291448f525138bc0145eb4c2007
JuuLe <lehtiljuuso@gmail.com>
2020-09-02 14:25:02 +0200

    Startade

---
+++ GruppUppgift2Grupp7VR20/Assets/ScoreCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class ScoreCounter : PlayableAsset
+{
+    public int Score = 0;
+    int NewPoints;
+
+    private void update()
+    {
+        Score + NewPoints;
+        NewPoints = 0;
+    }
+}
