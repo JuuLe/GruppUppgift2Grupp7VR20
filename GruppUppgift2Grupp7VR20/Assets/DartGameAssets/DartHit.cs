a2bf40fcf0e0abb792f87bbd48de4bfc35e83f1c
JuuLe <65231798+JuuLe@users.noreply.github.com>
2020-09-07 07:54:15 +0200

    Pilkastning klar

---
+++ GruppUppgift2Grupp7VR20/Assets/DartGameAssets/DartHit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartHit : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    //Ta kontroll över Rigidbody
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+
+    // Stoppa pilen när den träffar tavlan
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Piltavla"))
+        {
+            rb.isKinematic = true;
+        }
+    }
+}
